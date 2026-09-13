e96cea397c69efa9ba241dabca45e85a345598a4
cpufreestyle <cpufreestyle@users.noreply.github.com>
2026-09-13 10:12:35 +0800

    chore: bump version to 5.9.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.visionlink.android"
         minSdk = 31
         targetSdk = 35
-        versionCode = 599
-        versionName = "5.9.9"
+        versionCode = 59910
+        versionName = "5.9.10"
 
         multiDexEnabled = true
 
